3c56780ff3880bedf40d1050f5fee848f474ab4b
Vishal Srivastava <75941661+Dark8203@users.noreply.github.com>
2025-09-22 15:05:27 +0530

    updated vit.config

--- vite.config.ts
+++ vite.config.ts
@@ -3,17 +3,24 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      plugins: [react()],
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
+  const env = loadEnv(mode, '.', '');
+  return {
+    base: mode === 'production'
+      ? '/vero-website/'   // important: this must match the repo name
+      : '/',
+    build: {
+      outDir: 'dist',
+      assetsDir: 'assets',
+    },
+    plugins: [react()],
+    define: {
+      'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, '.'),
       }
-    };
+    }
+  };
 });
