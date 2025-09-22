37be675c22430d9589c39530e569b391a735e4bf
Vishal Srivastava <75941661+Dark8203@users.noreply.github.com>
2025-09-22 16:50:01 +0530

    new changes

--- vite.config.ts
+++ vite.config.ts
@@ -5,22 +5,16 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: mode === 'production'
-      ? '/vero-website/'   // important: this must match the repo name
-      : '/',
+    base: '/', // Set base to root for custom domain
     build: {
       outDir: 'dist',
       assetsDir: 'assets',
     },
     plugins: [react()],
-    define: {
-      'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-    },
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
-      }
-    }
+      },
+    },
   };
 });
