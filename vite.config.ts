cff53f1d24f3d3c960401c2540a82171b389a522
Alakh Agrawal <75619357+alakhagr@users.noreply.github.com>
2026-01-27 21:26:24 +0530

    update config custom domain
    update config custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      base: '/vero-website/', // ← Move this INSIDE the return object
+      base: '/', // ← Move this INSIDE the return object
       server: {
         port: 3000,
         host: '0.0.0.0',
